f8268fcbb8bb83ef696db169ff06ae2ae56c4433
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-10-29 18:43:21 +0000

    Tidy up page

--- app/page.tsx
+++ app/page.tsx
@@ -68,84 +68,87 @@ export default function Chat() {
   };
 
   return (
-    <div className="flex flex-col w-full h-screen max-w-md py-24 mx-auto stretch overflow-hidden">
-      <div className="space-y-4 bg-opacity-25 bg-gray-200 rounded-lg p-4">
-        <h3 className="text-xl font-semibold">Cuisine</h3>
-        {cuisines.map(({ value, emoji }) => (
-          <div
-            key={value}
-            className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
-          >
-            <input
-              id={value}
-              type="radio"
-              value={value}
-              name="cuisine"
-              onChange={handleChange}
-            />
-            <label className="ml-2" htmlFor={value}>
-              {`${emoji} ${value}`}
-            </label>
-          </div>
-        ))}
-      </div>
-
-      <div className="space-y-4 bg-opacity-25 bg-gray-200 rounded-lg p-4">
-        <h3 className="text-xl font-semibold">Type</h3>
-
-        {types.map(({ value, emoji }) => (
-          <div
-            key={value}
-            className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
-          >
-            <input
-              id={value}
-              type="radio"
-              value={value}
-              name="type"
-              onChange={handleChange}
-            />
-            <label className="ml-2" htmlFor={value}>
-              {`${emoji} ${value}`}
-            </label>
-          </div>
-        ))}
-      </div>
-
-      <div className="overflow-auto w-full" ref={messagesContainerRef}>
-        {messages.map((m) => (
-          <div
-            key={m.id}
-            className={`whitespace-pre-wrap ${
-              m.role === "user"
-                ? "bg-green-700 p-3 m-2 rounded-lg"
-                : "bg-slate-700 p-3 m-2 rounded-lg"
-            }`}
-          >
-            {m.role === "user" ? "User: " : "AI: "}
-            {m.content}
-          </div>
-        ))}
-      </div>
-
-      {isLoading && (
-        <div className="flex justify-end pr-4">
-          <span className="animate-pulse text-2xl">...</span>
+    <>
+      <div className="flex justify-center py-2">
+        <div className="space-y-4 bg-opacity-25 bg-gray-200 rounded-lg p-4 mx-2">
+          <h3 className="text-xl font-semibold">Cuisine</h3>
+          {cuisines.map(({ value, emoji }) => (
+            <div
+              key={value}
+              className="m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+            >
+              <input
+                id={value}
+                type="radio"
+                value={value}
+                name="cuisine"
+                onChange={handleChange}
+              />
+              <label className="ml-2" htmlFor={value}>
+                {`${emoji} ${value}`}
+              </label>
+            </div>
+          ))}
         </div>
-      )}
 
-      <button
-        className="bg-blue-500 p-2 text-white rounded shadow-xl"
-        disabled={isLoading}
-        onClick={() =>
-          append({
-            role: "user",
-            content: `Give me a ${state.cuisine} recipe which is ${state.type} food`,
-          })
-        }
-      >
-        Generate Recipe
-      </button>
-    </div>
+        <div className="space-y-4 bg-opacity-25 bg-gray-200 rounded-lg p-4 mx-2">
+          <h3 className="text-xl font-semibold">Type</h3>
+
+          {types.map(({ value, emoji }) => (
+            <div
+              key={value}
+              className="m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+            >
+              <input
+                id={value}
+                type="radio"
+                value={value}
+                name="type"
+                onChange={handleChange}
+              />
+              <label className="ml-2" htmlFor={value}>
+                {`${emoji} ${value}`}
+              </label>
+            </div>
+          ))}
+        </div>
+        <button
+          className="bg-green-800 p-2 text-white rounded shadow-xl mx-2"
+          disabled={isLoading}
+          onClick={() =>
+            append({
+              role: "user",
+              content: `Give me a recipe for a ${state.cuisine}, ${state.type} meal`,
+            })
+          }
+        >
+          Generate Recipe
+        </button>
+      </div>
+
+      <div className="flex flex-col w-5/6 h-screen py-8 mx-auto stretch overflow-hidden">
+        <div className="overflow-auto w-full" ref={messagesContainerRef}>
+          {messages.map((m) => (
+            <div
+              key={m.id}
+              className={`whitespace-pre-wrap ${
+                m.role === "user"
+                  ? "bg-green-700 p-3 m-2 rounded-lg"
+                  : "bg-slate-700 p-3 m-2 rounded-lg"
+              }`}
+            >
+              {m.role === "user" ? "User: " : "AI: "}
+              {m.content}
+            </div>
+          ))}
+        </div>
+
+        {isLoading && (
+          <div className="flex justify-end pr-4">
+            <span className="animate-pulse text-2xl">...</span>
+          </div>
+        )}
+      </div>
+    </>
   );
 }

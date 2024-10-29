299cdaf18911eac538625c2d7e7aa01d3ecdacd5
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-10-29 18:21:24 +0000

    Accept parameters to prompt

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import { useChat } from "ai/react";
-import { useEffect, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit, isLoading } =
-    useChat();
+  const { messages, isLoading, append } = useChat();
 
   const messagesContainerRef = useRef<HTMLDivElement>(null);
 
@@ -16,8 +15,103 @@ export default function Chat() {
     }
   }, [messages]);
 
+  const cuisines = [
+    {
+      emoji: "🇮🇹",
+      value: "Italian",
+    },
+    {
+      emoji: "🇻🇳",
+      value: "Vietnamese",
+    },
+    {
+      emoji: "🇫🇷",
+      value: "French",
+    },
+    {
+      emoji: "🇪🇹",
+      value: "Ethiopian",
+    },
+  ];
+
+  const types = [
+    {
+      emoji: "🥗",
+      value: "Healthy",
+    },
+    {
+      emoji: "🍲",
+      value: "Comfort",
+    },
+    {
+      emoji: "🧁",
+      value: "Dessert",
+    },
+    {
+      emoji: "🍔",
+      value: "Sandwich",
+    },
+  ];
+
+  const [state, setState] = useState({
+    cuisine: "",
+    type: "",
+  });
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="flex flex-col w-full h-screen max-w-md py-24 mx-auto stretch overflow-hidden">
+      <div className="space-y-4 bg-opacity-25 bg-gray-200 rounded-lg p-4">
+        <h3 className="text-xl font-semibold">Cuisine</h3>
+        {cuisines.map(({ value, emoji }) => (
+          <div
+            key={value}
+            className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+          >
+            <input
+              id={value}
+              type="radio"
+              value={value}
+              name="cuisine"
+              onChange={handleChange}
+            />
+            <label className="ml-2" htmlFor={value}>
+              {`${emoji} ${value}`}
+            </label>
+          </div>
+        ))}
+      </div>
+
+      <div className="space-y-4 bg-opacity-25 bg-gray-200 rounded-lg p-4">
+        <h3 className="text-xl font-semibold">Type</h3>
+
+        {types.map(({ value, emoji }) => (
+          <div
+            key={value}
+            className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+          >
+            <input
+              id={value}
+              type="radio"
+              value={value}
+              name="type"
+              onChange={handleChange}
+            />
+            <label className="ml-2" htmlFor={value}>
+              {`${emoji} ${value}`}
+            </label>
+          </div>
+        ))}
+      </div>
+
       <div className="overflow-auto w-full" ref={messagesContainerRef}>
         {messages.map((m) => (
           <div
@@ -40,16 +134,18 @@ export default function Chat() {
         </div>
       )}
 
-      <div className="fixed bottom-0 w-full max-w-md">
-        <form onSubmit={handleSubmit} className="flex justify-center">
-          <input
-            className="w-[95%] p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
-            value={input}
-            placeholder="Say something..."
-            onChange={handleInputChange}
-          />
-        </form>
-      </div>
+      <button
+        className="bg-blue-500 p-2 text-white rounded shadow-xl"
+        disabled={isLoading}
+        onClick={() =>
+          append({
+            role: "user",
+            content: `Give me a ${state.cuisine} recipe which is ${state.type} food`,
+          })
+        }
+      >
+        Generate Recipe
+      </button>
     </div>
   );
 }
